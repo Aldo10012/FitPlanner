b03c85bcf9c6d060c8d66b33817ca8f50104f252
Alberto <aldo10012@gmail.com>
2022-01-04 14:47:18 -0800

    [ADDED] calendar

--- FitPlanner/AppModules/Controller/FirstVC.swift
+++ FitPlanner/AppModules/Controller/FirstVC.swift
@@ -6,12 +6,17 @@
 //
 
 import UIKit
+import FSCalendar
 
 class FirstVC: UIViewController {
     
     // MARK: Properties
     let nextWorkoutLabel = FPLabel(title: "Your next workout", color: .primary, size: 18, weight: .medium)
     let nextWorkoutCard = NextWorkoutCard()
+    let yourActivityLabel = FPLabel(title: "Your activity", color: .primary, size: 18, weight: .medium)
+    weak var calendar: FSCalendar!
+    
+    var formatter = DateFormatter()
 
     
     // MARK: Life Cycles
@@ -19,7 +24,7 @@ class FirstVC: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = .FPBackground
-        Router.presentController(from: self, to: GetStartedVC(), animated: false)
+        //Router.presentController(from: self, to: GetStartedVC(), animated: false)
         
         setupUI()
     }
@@ -33,7 +38,43 @@ class FirstVC: UIViewController {
         
         view.addSubview(nextWorkoutCard)
         nextWorkoutCard.anchor(top: nextWorkoutLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 12, paddingLeft: 20, paddingRight: 20)
+        
+        view.addSubview(yourActivityLabel)
+        yourActivityLabel.anchor(top: nextWorkoutCard.bottomAnchor, left: view.leftAnchor, paddingTop: 32, paddingLeft: 20)
+        
+        // calendar setup
+        setupCalendar()
+    }
+    
+    fileprivate func setupCalendar() {
+        let calendarbackground = FPBackground(radius: 8)
+        view.addSubview(calendarbackground)
+        calendarbackground.anchor(top: yourActivityLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 12, paddingLeft: 20, paddingRight: 20, height: 320)
+        calendarbackground.addShadow(color: .black, opacity: 0.15, x: 0, y: 4, radius: 8)
+        
+        let calendar = FSCalendar(frame: CGRect(x: 20, y: 10, width: view.frame.size.width - 80, height: 300))
+        calendar.dataSource = self
+        calendar.delegate = self
+        
+        calendar.appearance.headerTitleColor = .FPBlue
+        calendar.appearance.weekdayTextColor = .FPTextFieldPlaceholder
+        calendar.appearance.todayColor = .FPBlue
+        calendar.appearance.titleTodayColor = .white
+        // calendar.pagingEnabled = false   ~ this makes the calendar NOT scrollable
+        calendarbackground.addSubview(calendar)
+        self.calendar = calendar
     }
 
+}
+
+
+// MARK: Celendar Delegates
+extension FirstVC: FSCalendarDataSource, FSCalendarDelegate{
+    
+    public func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        formatter.dateFormat = "dd-MMM-yyyy"
+        print("Date Selected: \(formatter.string(from: date))")
+    }
+    
 }
 
---
+++ FitPlanner/Helpers/ScreenSize.swift
@@ -0,0 +1,13 @@
+//
+//  ScreenSize.swift
+//  FitPlanner
+//
+//  Created by Alberto Dominguez on 1/4/22.
+//
+
+import UIKit
+
+struct ScreenSize {
+    static let width: CGFloat = UIView().frame.size.width
+    static let height: CGFloat = UIView().frame.size.height
+}
