e318133a368a30ac06fb147080211d8ddf408f1a
Alberto <aldo10012@gmail.com>
2022-01-05 14:37:44 -0800

    [ADDED] CoreData entities + minor reorganizing

---
+++ FitPlanner/AppModules/ViewModel/For Controllers/HomeVM.swift
@@ -0,0 +1,13 @@
+//
+//  HomeVM.swift
+//  FitPlanner
+//
+//  Created by Alberto Dominguez on 1/5/22.
+//
+
+import Foundation
+
+struct HomeVM {
+    let nextWorkout: String // TODO: change type
+    let activity: String    // TODO: change type
+}
