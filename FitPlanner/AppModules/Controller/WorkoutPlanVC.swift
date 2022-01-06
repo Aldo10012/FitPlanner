54a210873c256b49c875bb4f22e7247b4e7a192d
Alberto <aldo10012@gmail.com>
2022-01-05 18:38:38 -0800

    [REFACTORED] AddExerciseCardView

--- FitPlanner/AppModules/Controller/WorkoutPlanVC.swift
+++ FitPlanner/AppModules/Controller/WorkoutPlanVC.swift
@@ -20,8 +20,8 @@ class WorkoutPlanVC: UIViewController {
     
     var workoutNameTextField = FPTextField(placeholder: "Workout name", size: 30, weight: .light)
     var workoutcardView: WorkoutCardView!
-    var addExerciseCard = FPBackground(radius: 8)
-    var tableView = UITableView()
+    var addExerciseCard = AddExerciseCardView()//FPBackground(radius: 8)
+//    var tableView = UITableView()
     var button = FPButton(type: .primary, title: "Button")
     
     
@@ -63,50 +63,23 @@ class WorkoutPlanVC: UIViewController {
         view.backgroundColor = .FPBackground
         title = "Workout Plan"
         
-        view.addSubview(workoutNameTextField)
+        view.addSubviews(workoutNameTextField, workoutcardView, button, addExerciseCard)
+        
         workoutNameTextField.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, paddingTop: 30, paddingLeft: 20)
         
-        view.addSubview(workoutcardView)
         workoutcardView.anchor(top: workoutNameTextField.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor,
                                paddingTop: 30, paddingLeft: 20, paddingRight: 30)
         
-        view.addSubview(button)
         button.anchor(left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingLeft: 20, paddingBottom: 20, paddingRight: 20)
         
-        setupAddExerciseCard()
-    }
-    
-    fileprivate func setupAddExerciseCard() {
-        view.addSubviews(addExerciseCard)
         addExerciseCard.addStandardShadow()
         addExerciseCard.anchor(top: workoutcardView.bottomAnchor, left: workoutcardView.leftAnchor, right: workoutcardView.rightAnchor, paddingTop: 30, height: 250)
         
-        // Add subviews to card
-        // adding labels
-        let mainLabel = FPLabel(title: "Exercises", color: .primary, size: 18, weight: .light, alignment: .center)
-        let numberLabel = FPLabel(title: "#", color: .primary, size: 16, weight: .light, alignment: .center)
-        let nameLabel = FPLabel(title: "name", color: .primary, size: 16, weight: .light, alignment: .center)
-        let repsLabel = FPLabel(title: "reps", color: .primary, size: 16, weight: .light, alignment: .center)
-        let setsLabel = FPLabel(title: "sets", color: .primary, size: 16, weight: .light, alignment: .center)
-        
-        addExerciseCard.addSubviews(mainLabel, numberLabel, nameLabel, repsLabel, setsLabel)
-        mainLabel.anchor(top: addExerciseCard.topAnchor, left: addExerciseCard.leftAnchor, paddingTop: 12, paddingLeft: 15)
-        numberLabel.anchor(top: mainLabel.bottomAnchor, left: mainLabel.leftAnchor, paddingTop: 22)
-        nameLabel.anchor(top: numberLabel.topAnchor, left: numberLabel.leftAnchor, paddingLeft: 18)
-        setsLabel.anchor(top: numberLabel.topAnchor, right: addExerciseCard.rightAnchor, paddingRight: 12, width: 60)
-        repsLabel.anchor(top: numberLabel.topAnchor, right: setsLabel.leftAnchor, paddingRight: 0, width: 60)
-        
-        // adding tableView
-        addExerciseCard.addSubview(tableView)
-        tableView.anchor(top: numberLabel.bottomAnchor, left: numberLabel.leftAnchor, bottom: addExerciseCard.bottomAnchor, right: setsLabel.rightAnchor, paddingTop: 15, paddingBottom: 15)
-        
-        tableView.register(ExerciseTableViewCell.self, forCellReuseIdentifier: CellId.exerciseCell)
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.separatorStyle = .none
-        
-        
+        addExerciseCard.tableView.delegate = self
+        addExerciseCard.tableView.dataSource = self
     }
+    
+    
     
     // MARK: Check type
     fileprivate func modifyViewBasedOnType(viewModel: WorkoutCardVM) {
---
+++ FitPlanner/AppModules/View/Container Views/AddExerciseCardView.swift
@@ -0,0 +1,51 @@
+//
+//  AddExerciseCard.swift
+//  FitPlanner
+//
+//  Created by Alberto Dominguez on 1/5/22.
+//
+
+import UIKit
+
+class AddExerciseCardView: UIView {
+
+    // MARK: Properties
+    let mainLabel = FPLabel(title: "Exercises", color: .primary, size: 18, weight: .light, alignment: .center)
+    let numberLabel = FPLabel(title: "#", color: .primary, size: 16, weight: .light, alignment: .center)
+    let nameLabel = FPLabel(title: "name", color: .primary, size: 16, weight: .light, alignment: .center)
+    let repsLabel = FPLabel(title: "reps", color: .primary, size: 16, weight: .light, alignment: .center)
+    let setsLabel = FPLabel(title: "sets", color: .primary, size: 16, weight: .light, alignment: .center)
+    var tableView = UITableView()
+    
+    
+    // MARK: Init
+    init() {
+        super.init(frame: .zero)
+        setupViews()
+        backgroundColor = .white
+        layer.cornerRadius = 8
+    }
+    
+    // MARK: Helpers
+    fileprivate func setupViews() {
+        addSubviews(mainLabel, numberLabel, nameLabel, repsLabel, setsLabel)
+        mainLabel.anchor(top: topAnchor, left: leftAnchor, paddingTop: 12, paddingLeft: 15)
+        numberLabel.anchor(top: mainLabel.bottomAnchor, left: mainLabel.leftAnchor, paddingTop: 22)
+        nameLabel.anchor(top: numberLabel.topAnchor, left: numberLabel.leftAnchor, paddingLeft: 18)
+        setsLabel.anchor(top: numberLabel.topAnchor, right: rightAnchor, paddingRight: 12, width: 60)
+        repsLabel.anchor(top: numberLabel.topAnchor, right: setsLabel.leftAnchor, paddingRight: 0, width: 60)
+        
+        // adding tableView
+        addSubview(tableView)
+        tableView.anchor(top: numberLabel.bottomAnchor, left: numberLabel.leftAnchor, bottom: bottomAnchor, right: setsLabel.rightAnchor, paddingTop: 15, paddingBottom: 15)
+        
+        tableView.register(ExerciseTableViewCell.self, forCellReuseIdentifier: CellId.exerciseCell)
+//        tableView.delegate = self
+//        tableView.dataSource = self
+        tableView.separatorStyle = .none
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
