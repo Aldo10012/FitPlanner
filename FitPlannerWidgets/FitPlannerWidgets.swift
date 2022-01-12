1b9a33d2ad55dced70200b944b94b35e4f3e64ef
Alberto <aldo10012@gmail.com>
2022-01-12 12:50:44 -0800

    [ADDED] small widget view and viewModel - CoreData not fetching

--- FitPlannerWidgets/FitPlannerWidgets.swift
+++ FitPlannerWidgets/FitPlannerWidgets.swift
@@ -40,9 +40,51 @@ struct SimpleEntry: TimelineEntry {
 
 struct FitPlannerWidgetsEntryView : View {
     var entry: Provider.Entry
+    var viewModel = SmallWidgetViewModel()
 
     var body: some View {
-        Text(entry.date, style: .time)
+        HStack {
+            VStack(alignment: .leading) {
+                Text("Next Workout")
+                    .font(.system(size: 14, weight: .medium, design: .default))
+                    .foregroundColor(Color(UIColor.FPBlue))
+                    .padding(.bottom, 5)
+                
+                Text(viewModel.nextWorkoutName)
+                    .font(.system(size: 18, weight: .light, design: .default))
+                    .foregroundColor(Color(UIColor.FPOffBlack))
+                    .padding(.bottom, 20)
+                
+                Text(viewModel.nextWorkoutDate)
+                    .font(.system(size: 16, weight: .regular, design: .default))
+                    .foregroundColor(Color(UIColor.FPTextFieldPlaceholder))
+                    .padding(.bottom, 10)
+                Spacer()
+            }
+            Spacer()
+        }.padding()
+            
+        
+    }
+}
+
+class SmallWidgetViewModel: ObservableObject {
+    let myData = CoreDataStack.shared
+    @Published var nextWorkoutName = ""
+    @Published var nextWorkoutDate = ""
+    init() {
+        getNextWorkout()
+    }
+    func getNextWorkout() {
+        guard let nextWorkout = myData.getNextWorkout() else {
+            nextWorkoutName = "Workout Name"
+            nextWorkoutDate = "Today, \nJun 12"
+            return
+        }
+        print(nextWorkout)
+        nextWorkoutName = nextWorkout.name!
+        nextWorkoutDate = getDateAsStringLong(dateOfNextWorkout)
+        
     }
 }
 
